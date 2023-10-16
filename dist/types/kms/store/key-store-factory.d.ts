dcaac2a4a52873faf62601fc00caea310b60d24c
vbasiuk <volodymyr.basiuck@gmail.com>
2023-10-16 19:38:06 +0300

    rm dist

--- dist/types/kms/store/key-store-factory.d.ts
+++
@@ -1,6 +0,0 @@
-import { Document } from 'mongodb';
-import { MongoDBPrivateKeyStore } from './key-store';
-/**
- * Returns instance of MongoDBPrivateKeyStore
- */
-export declare const MongoDBPrivateKeyStoreFactory: <Type extends Document>(url: string, dbName: string, collection?: string) => Promise<MongoDBPrivateKeyStore>;
